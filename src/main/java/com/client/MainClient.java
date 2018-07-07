9a52e12f89bacc4cae3614d2c4738f58e1b4b1f7
sebastiano.anchise <sebastiano.anchise@10.2.2.1>
2018-07-07 22:24:25 +0200

    Add MainClient

---
+++ src/main/java/com/client/MainClient.java
@@ -0,0 +1,44 @@
+package com.client;
+
+import java.time.Duration;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisPubSub;
+
+public class MainClient {
+
+	static JedisPool jp;
+	
+	public static void main(String[] args) {
+		
+		jp = new JedisPool(buildPoolConfig(),"172.30.118.49", 6379);
+		Jedis jClient = jp.getResource();
+		jClient.auth(System.getenv("REDIS_PWD"));
+		jClient.connect();
+		jClient.subscribe(new JedisPubSub() {
+		    @Override
+		    public void onMessage(String channel, String message) {
+		        System.out.println(jClient.rpop("Users"));
+		    }
+		}, "Nuovi_Utenti");
+	}
+
+	private static JedisPoolConfig buildPoolConfig() {
+	    final JedisPoolConfig poolConfig = new JedisPoolConfig();
+	    poolConfig.setMaxTotal(128);
+	    poolConfig.setMaxIdle(128);
+	    poolConfig.setMinIdle(16);
+	    poolConfig.setTestOnBorrow(true);
+	    poolConfig.setTestOnReturn(true);
+	    poolConfig.setTestWhileIdle(true);
+	    poolConfig.setMinEvictableIdleTimeMillis(Duration.ofSeconds(60).toMillis());
+	    poolConfig.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(30).toMillis());
+	    poolConfig.setNumTestsPerEvictionRun(3);
+	    poolConfig.setBlockWhenExhausted(true);
+	    return poolConfig;
+	}
+	
+	
+}
